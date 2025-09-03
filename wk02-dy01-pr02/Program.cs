62ae7988bd1cf16524898531741b7b83d26adebd
TheCoolGuyisInTheHouse <clrobin4321@outlook.com>
2025-09-03 13:43:32 -0400

    Completed C# 02 Exercise 02

--- wk02-dy01-pr02/Program.cs
+++ wk02-dy01-pr02/Program.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b38a790583a4e0e45b029779e0831d2926cfe7b2075d23729a9afbfa34f0c0a
-size 2681
+oid sha256:54b74bb10544cb0857eb0ece47b0d7b3be7f7b7c661e616df8be49103e15c01f
+size 2891
--- wk02-dy01-pr02/obj/Debug/net8.0/wk02-dy01-pr02.AssemblyInfo.cs
+++ wk02-dy01-pr02/obj/Debug/net8.0/wk02-dy01-pr02.AssemblyInfo.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7be5a468386713597be781d28a7493160d496c39976f7ec14ec9d547ba6b6d06
+oid sha256:c5a240611514cb2f9b29b5a5f8dfeea83d04f6fecb75c1c12ab0127892412806
 size 1033
