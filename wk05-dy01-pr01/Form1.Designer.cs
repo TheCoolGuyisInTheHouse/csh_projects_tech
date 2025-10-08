2e6a6536dae8be63c2dd25b5d61a40a96758477e
TheCoolGuyisInTheHouse <clrobin4321@outlook.com>
2025-10-08 14:07:10 -0400

    Continued C# 05 Exercise 01

--- wk05-dy01-pr01/Form1.Designer.cs
+++ wk05-dy01-pr01/Form1.Designer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:913919e36c72a941758ab52331d85e0450a781cdfbd3200dd423194061fdccbe
-size 3053
+oid sha256:54ca1f22930635122f09f3ab35f354c8e4f738cd2d4af6b3dd83af424173cfa7
+size 3541
--- wk05-dy01-pr01/Form1.cs
+++ wk05-dy01-pr01/Form1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3fc586d368a2141eefe1c19f1e1495ad84bb9c2a13b6410e331d94f21b6e051
-size 169
+oid sha256:8db4b641741de3dab97872939075f51f82ab963204e6a8b96554d2503dd58093
+size 254
